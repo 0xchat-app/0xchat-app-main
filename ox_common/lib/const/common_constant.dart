85ba98bd7358fbd9f51ed8296ab4b313e9c6c3d9
bear-Wx <562455044@qq.com>
2023-08-25 17:11:03 +0800

    [bear][add] 1. add images

--- ox_common/lib/const/common_constant.dart
+++ ox_common/lib/const/common_constant.dart
@@ -6,7 +6,7 @@ import 'common_constant_interface.dart';
 // As a reference, you can use 'common_constant_example.dart' to see the expected fields and their format.
 // Once you have your implementation, create a 'common_constant_0xchat.dart' file and ensure it has a class named 'OXConstantImpl' that implements 'ConstantInterface'.
 
-import 'config/common_constant_0xchat.dart';
+import '0xchat-config-private/common_constant_0xchat.dart';
 
 // Uncomment the line below and comment out the above import if you want to use the example as a placeholder.
 // import 'common_constant_example.dart';
