f6372817b3a4efbdbfa735602cb4eb6ba12eec56
b-fab-michael <john1995789@gmail.com>
2023-09-22 11:31:35 +0800

    [Michael][update] 1、add sound; 2、calling mothed

--- ox_common/lib/utils/chat_prompt_tone.dart
+++ ox_common/lib/utils/chat_prompt_tone.dart
@@ -16,6 +16,20 @@ class PromptToneManager {
   }
 
   void play() async {
-    await _player.play(AssetSource('sounds/message_notice_sound.mp3'));
+    if (_player.state != PlayerState.playing) {
+      _player.play(AssetSource('sounds/message_notice_sound.mp3'));
+    }
+  }
+
+  void playCalling() async {
+    _player.stop();
+    _player.setReleaseMode(ReleaseMode.loop);
+    _player.play(AssetSource('sounds/sound_calling_3d.mp3'));
+  }
+
+  void stopPlay() async {
+    if ( _player.state == PlayerState.playing) {
+      _player.stop();
+    }
   }
 }
