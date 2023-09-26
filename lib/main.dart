b669f16cf90b8df019cbb125d6a4025803c791b8
Justin <Justin@gmail.com>
2023-09-26 14:57:32 +0800

    [Justin][update]setup oxchat home

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:orientation/orientation.dart';
 import 'package:ox_common/const/common_constant.dart';
+import 'package:ox_home/ox_home.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ox_chat/ox_chat.dart';
 import 'package:ox_common/event_bus.dart';
@@ -115,6 +116,7 @@ Future<void> setupModules() async {
   OXChat().setup();
   OXChatUI().setup();
   OxCalling().setup();
+  OxChatHome().setup();
 }
 
 class MainApp extends StatefulWidget {
