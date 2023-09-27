64c584960516f97a30879e82a2f947412ae7f4d2
ZharlieW <zhw306498704@gmail.com>
2023-09-27 15:09:39 +0800

    [Zzz][update] 1. Modify enum value. 2. Commented out unused methods.

--- ox_common/lib/business_interface/ox_chat/interface.dart
+++ ox_common/lib/business_interface/ox_chat/interface.dart
@@ -2,22 +2,20 @@
 import 'package:ox_common/model/chat_session_model.dart';
 import 'package:ox_module_service/ox_module_service.dart';
 
-import 'call_message_type.dart';
-
 class OXChatInterface {
 
   static const moduleName = 'ox_chat';
 
-  static void sendCallMessage(ChatSessionModel session, String text, CallMessageType type, String? authorPubkey,) {
-    OXModuleService.invoke(
-      moduleName,
-      'sendCallMessage',
-      [],
-      {
-        #session: session,
-        #text: text,
-        #type: type,
-        #authorPubkey: authorPubkey,
-      },);
-  }
+  // static void sendCallMessage(ChatSessionModel session, String text, CallMessageType type, String? authorPubkey,) {
+  //   OXModuleService.invoke(
+  //     moduleName,
+  //     'sendCallMessage',
+  //     [],
+  //     {
+  //       #session: session,
+  //       #text: text,
+  //       #type: type,
+  //       #authorPubkey: authorPubkey,
+  //     },);
+  // }
 }
