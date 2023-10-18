1bdb08f82df382dff08b8e985083d82db8d7164a
water783 <wkuangji@outlook.com>
2023-10-18 15:12:24 +0800

    upgrade dbVersion to 5

--- ox_common/lib/const/common_constant_interface.dart
+++ ox_common/lib/const/common_constant_interface.dart
@@ -4,7 +4,7 @@ abstract class ConstantInterface {
   const ConstantInterface();
 
   ///db version
-  int get dbVersion => 4;
+  int get dbVersion => 5;
 
   /// 0xchat relay
   String get oxChatRelay => 'wss://relay.0xchat.com';
