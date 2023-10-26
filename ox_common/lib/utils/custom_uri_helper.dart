a30fb6b80da618304e4b74c8452293866bca67c4
ZharlieW <zhw306498704@gmail.com>
2023-10-26 11:31:07 +0800

    [Zzz][update] 1. Add tryHandleCustomUri on CustomURIActionEx.

--- ox_common/lib/utils/custom_uri_helper.dart
+++ ox_common/lib/utils/custom_uri_helper.dart
@@ -1,6 +1,10 @@
 
 import 'dart:convert';
 
+import 'package:flutter/cupertino.dart';
+import 'package:ox_common/log_util.dart';
+import 'package:ox_module_service/ox_module_service.dart';
+
 extension CustomURIHelper on String {
 
   static const scheme = 'oxChat';
@@ -50,5 +54,29 @@ extension CustomURIHelper on String {
 
   static String _createURI({required String host, Map<String, String>? queryParameters}) {
     return Uri(scheme: scheme, host: host, queryParameters: queryParameters).toString();
+  }
+}
+
+extension CustomURIActionEx on String {
+  dynamic tryHandleCustomUri({BuildContext? context}) {
+    if (isModuleActionURI) {
+      return _doModuleAction(context: context);
+    }
+  }
+
+  dynamic _doModuleAction({BuildContext? context}) {
+    final result = getModuleActionValue();
+    if (result == null) {
+      LogUtil.e('【Error】CustomURIActionEx tryAction error: result is null.');
+      return;
+    }
+    final module = result.module;
+    final action = result.action;
+    final params = result.params.map((key, value) => MapEntry(Symbol(key), value));
+    try {
+      return OXModuleService.invoke(module, action, [context], params);
+    } catch(e) {
+      LogUtil.e('【Error】CustomURIActionEx tryAction error: $e.');
+    }
   }
 }
