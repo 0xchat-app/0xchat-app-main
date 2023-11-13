afbf3ae74132da150dce590800c2c8a497ae8262
ZharlieW <zhw306498704@gmail.com>
2023-11-13 18:16:17 +0800

    [Zzz][update] 1. Add 'scan_path' native code.

---
+++ ox_common/ios/Classes/OXCQRCodeHelper.swift
@@ -0,0 +1,61 @@
+//
+//  OXCQRCodeHelper.swift
+//  ox_common
+//
+//  Created by ZharlieW on 2023/11/13.
+//
+
+import UIKit
+import Vision
+
+class OXCQRCodeHelper {
+    
+    static func detectQRCode(_ image: UIImage?) -> [CIFeature]? {
+        if let image = image, let ciImage = CIImage.init(image: image){
+            var options: [String: Any];
+            let context = CIContext();
+            options = [CIDetectorAccuracy: CIDetectorAccuracyHigh];
+            let qrDetector = CIDetector(ofType: CIDetectorTypeQRCode, context: context, options: options);
+            if ciImage.properties.keys.contains((kCGImagePropertyOrientation as String)){
+                options = [CIDetectorImageOrientation: ciImage.properties[(kCGImagePropertyOrientation as String)] ?? 1];
+            } else {
+                options = [CIDetectorImageOrientation: 1];
+            }
+            let features = qrDetector?.features(in: ciImage, options: options);
+            return features;
+        }
+        return nil
+    }
+    
+    static func detectBarCode(_ image: UIImage?, result: @escaping FlutterResult) {
+        if let image = image, let ciImage = CIImage.init(image: image), #available(iOS 11.0, *) {
+            var requestHandler: VNImageRequestHandler;
+            if ciImage.properties.keys.contains((kCGImagePropertyOrientation as String)) {
+                requestHandler = VNImageRequestHandler(ciImage: ciImage, orientation: CGImagePropertyOrientation(rawValue: ciImage.properties[(kCGImagePropertyOrientation as String)] as! UInt32) ?? .up, options: [:])
+            } else {
+                requestHandler = VNImageRequestHandler(ciImage: ciImage, orientation: .up, options: [:])
+            }
+            let request = VNDetectBarcodesRequest { (request,error) in
+                var res: String? = nil;
+                if let observations = request.results as? [VNBarcodeObservation], !observations.isEmpty {
+                    let data: VNBarcodeObservation = observations.first!;
+                    res = data.payloadStringValue;
+                }
+                DispatchQueue.main.async {
+                    result(res);
+                }
+            }
+            DispatchQueue.global(qos: .background).async {
+                do{
+                    try requestHandler.perform([request])
+                } catch {
+                    DispatchQueue.main.async {
+                        result(nil);
+                    }
+                }
+            }
+        } else {
+            result(nil);
+        }
+    }
+}
