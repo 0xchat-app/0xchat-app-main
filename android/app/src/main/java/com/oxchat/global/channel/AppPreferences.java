2147451e6415577111c480c68e2588b6caf92d3a
b-fab-michael <john1995789@gmail.com>
2023-11-29 15:37:04 +0800

    [Michael][update] 1、signature encryption for Amber

--- android/app/src/main/java/com/oxchat/global/channel/AppPreferences.java
+++ android/app/src/main/java/com/oxchat/global/channel/AppPreferences.java
@@ -1,27 +1,30 @@
 package com.oxchat.global.channel;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.database.Cursor;
 import android.net.Uri;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 
+import com.dexterous.flutterlocalnotifications.utils.LongUtils;
 import com.oxchat.global.MultiEngineActivity;
 import com.oxchat.global.util.SharedPreUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
+import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.PluginRegistry;
 
 /**
  * Title: ApplicationPreferences
@@ -32,10 +35,12 @@ import io.flutter.plugin.common.MethodChannel;
  * @CheckItem Fill in by oneself
  * @since JDK1.8
  */
-public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware {
+public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware, PluginRegistry.ActivityResultListener {
     private static final String OX_PERFERENCES_CHANNEL = "com.oxchat.global/perferences";
     private Context mContext;
     private Activity mActivity;
+    private MethodChannel.Result mMethodChannelResult;
+    private int mSignatureRequestCode = 101;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
@@ -52,6 +57,8 @@ public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterP
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         mActivity = binding.getActivity();
+        binding.addActivityResultListener(this);
+
     }
 
     @Override
@@ -71,6 +78,7 @@ public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterP
 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull MethodChannel.Result result) {
+        mMethodChannelResult = result;
         HashMap paramsMap = null;
         if (call.arguments != null && call.arguments instanceof HashMap) {
             paramsMap = (HashMap) call.arguments;
@@ -88,7 +96,7 @@ public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterP
             SharedPreferences preferences = mContext.getSharedPreferences(SharedPreUtils.SP_NAME, Context.MODE_PRIVATE);
             preferences.edit().putInt("themeStyle", themeStyle);
             if (themeStyle == 0) {
-               //TODO light
+                //TODO light
             } else {
                 //TODO Dark
             }
@@ -106,40 +114,67 @@ public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterP
                     .withNewEngine(MultiEngineActivity.class)
                     .initialRoute(MultiEngineActivity.getFullRoute(route, params))
                     .build(mContext);
-//            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            //            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             mActivity.startActivity(intent);
-        } else if (call.method.equals("getPubKey")){
-            List<String> proList = new ArrayList<>();
-            proList.add("login");
-            proList.add("测试");
-            String[] stringArray = proList.toArray(new String[0]);
-            for (String s : stringArray) {
-                Log.e("Michael", "数组的值----s ="+s);
+        } else if (call.method.equals("nostrsigner")) {
+            String extendParse = "";
+            if (paramsMap.containsKey("extendParse")) {
+                extendParse = (String) paramsMap.get("extendParse");
             }
-            Cursor tempResult = mContext.getContentResolver().query(
-                    Uri.parse("content://com.greenart7c3.nostrsigner.GET_PUBLIC_KEY"),
-                    proList.toArray(new String[0]), null,null,null
-            );
-            if (tempResult != null) {
-                try {
-                    while (tempResult.moveToNext()) {
-                        int columnIndex = tempResult.getColumnIndex("login");
-                        if (columnIndex >= 0) {
-                            // 确保列索引有效
-                            String login = tempResult.getString(columnIndex);
-                            Log.e("Michael", "----login ="+login);
-                            // 此处处理 login 变量
-                        } else {
-                            // 列 "login" 在结果中不存在
-                            // 此处处理错误或执行替代操作
-                        }
-                    }
-                } finally {
-                    tempResult.close();
-                }
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("nostrsigner:" + extendParse));
+            intent.setPackage("com.greenart7c3.nostrsigner");
+            String type = "get_public_key";
+            if (paramsMap.containsKey("type")) {
+                type = (String) paramsMap.get("type");
             }
+            intent.putExtra("type", type);
+            if (paramsMap.containsKey("id")) {
+                String id = (String) paramsMap.get("id");
+                intent.putExtra("id", id);
+            }
+            if (paramsMap.containsKey("current_user")) {
+                String current_user = (String) paramsMap.get("current_user");
+                intent.putExtra("current_user", current_user);
+            }
+            if (paramsMap.containsKey("pubKey")) {
+                String pubKey = (String) paramsMap.get("pubKey");
+                intent.putExtra("pubKey", pubKey);
+            }
+            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            if (paramsMap.containsKey("requestCode")) {
+                mSignatureRequestCode = (int) paramsMap.get("requestCode");
+            }
+            mActivity.startActivityForResult(intent, mSignatureRequestCode);
+
+
         }
     }
 
-
+    public boolean onActivityResult(int requestCode, int resultCode, Intent result) {
+        if (mSignatureRequestCode == requestCode) {
+            if (resultCode == RESULT_OK && result != null) {
+                Map<String, String> dataMap = new HashMap<>();
+                if (result.hasExtra("signature")) {
+                    String signature = result.getStringExtra("signature");
+                    dataMap.put("signature", signature);
+                }
+                if (result.hasExtra("id")) {
+                    String id = result.getStringExtra("id");
+                    dataMap.put("id", id);
+                }
+                if (result.hasExtra("event")) {
+                    String event = result.getStringExtra("event");
+                    dataMap.put("event", event);
+                }
+                if (mMethodChannelResult != null) {
+                    mMethodChannelResult.success(dataMap);
+                }
+            } else {
+                if (mMethodChannelResult != null) {
+                    mMethodChannelResult.success(null);
+                }
+            }
+        }
+        return false;
+    }
 }
