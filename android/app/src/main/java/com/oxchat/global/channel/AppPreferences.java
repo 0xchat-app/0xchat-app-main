601c05b2984d8820f20419af49ed2148f54f192f
b-fab-michael <john1995789@gmail.com>
2023-11-29 18:45:23 +0800

    [Michael][update] 1、login with Amber move to core

--- android/app/src/main/java/com/oxchat/global/channel/AppPreferences.java
+++ android/app/src/main/java/com/oxchat/global/channel/AppPreferences.java
@@ -1,30 +1,22 @@
 package com.oxchat.global.channel;
 
-import static android.app.Activity.RESULT_OK;
-
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.util.Log;
 
 import androidx.annotation.NonNull;
 
-import com.dexterous.flutterlocalnotifications.utils.LongUtils;
 import com.oxchat.global.MultiEngineActivity;
 import com.oxchat.global.util.SharedPreUtils;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
 import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding;
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.PluginRegistry;
 
 /**
  * Title: ApplicationPreferences
@@ -35,12 +27,11 @@ import io.flutter.plugin.common.PluginRegistry;
  * @CheckItem Fill in by oneself
  * @since JDK1.8
  */
-public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware, PluginRegistry.ActivityResultListener {
+public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterPlugin, ActivityAware {
     private static final String OX_PERFERENCES_CHANNEL = "com.oxchat.global/perferences";
     private Context mContext;
     private Activity mActivity;
     private MethodChannel.Result mMethodChannelResult;
-    private int mSignatureRequestCode = 101;
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
@@ -57,7 +48,6 @@ public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterP
     @Override
     public void onAttachedToActivity(@NonNull ActivityPluginBinding binding) {
         mActivity = binding.getActivity();
-        binding.addActivityResultListener(this);
 
     }
 
@@ -116,65 +106,7 @@ public class AppPreferences implements MethodChannel.MethodCallHandler, FlutterP
                     .build(mContext);
             //            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
             mActivity.startActivity(intent);
-        } else if (call.method.equals("nostrsigner")) {
-            String extendParse = "";
-            if (paramsMap.containsKey("extendParse")) {
-                extendParse = (String) paramsMap.get("extendParse");
-            }
-            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("nostrsigner:" + extendParse));
-            intent.setPackage("com.greenart7c3.nostrsigner");
-            String type = "get_public_key";
-            if (paramsMap.containsKey("type")) {
-                type = (String) paramsMap.get("type");
-            }
-            intent.putExtra("type", type);
-            if (paramsMap.containsKey("id")) {
-                String id = (String) paramsMap.get("id");
-                intent.putExtra("id", id);
-            }
-            if (paramsMap.containsKey("current_user")) {
-                String current_user = (String) paramsMap.get("current_user");
-                intent.putExtra("current_user", current_user);
-            }
-            if (paramsMap.containsKey("pubKey")) {
-                String pubKey = (String) paramsMap.get("pubKey");
-                intent.putExtra("pubKey", pubKey);
-            }
-            intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-            if (paramsMap.containsKey("requestCode")) {
-                mSignatureRequestCode = (int) paramsMap.get("requestCode");
-            }
-            mActivity.startActivityForResult(intent, mSignatureRequestCode);
-
-
         }
     }
 
-    public boolean onActivityResult(int requestCode, int resultCode, Intent result) {
-        if (mSignatureRequestCode == requestCode) {
-            if (resultCode == RESULT_OK && result != null) {
-                Map<String, String> dataMap = new HashMap<>();
-                if (result.hasExtra("signature")) {
-                    String signature = result.getStringExtra("signature");
-                    dataMap.put("signature", signature);
-                }
-                if (result.hasExtra("id")) {
-                    String id = result.getStringExtra("id");
-                    dataMap.put("id", id);
-                }
-                if (result.hasExtra("event")) {
-                    String event = result.getStringExtra("event");
-                    dataMap.put("event", event);
-                }
-                if (mMethodChannelResult != null) {
-                    mMethodChannelResult.success(dataMap);
-                }
-            } else {
-                if (mMethodChannelResult != null) {
-                    mMethodChannelResult.success(null);
-                }
-            }
-        }
-        return false;
-    }
 }
