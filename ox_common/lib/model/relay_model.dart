40e3de8bd5426c79688426f0bee146f8f8d330bf
b-fab-michael <john1995789@gmail.com>
2023-11-30 17:13:53 +0800

    [Michael][update] 1、updated relay‘s logic for deletion and addition

--- ox_common/lib/model/relay_model.dart
+++ ox_common/lib/model/relay_model.dart
@@ -1,3 +1,4 @@
+import 'package:ox_common/const/common_constant.dart';
 
 ///Title: relay_model
 ///Description: TODO(Fill in by oneself)
@@ -5,7 +6,7 @@
 ///@author Michael
 ///CreateTime: 2023/5/5 15:57
 
-class RelayModel{
+class RelayModel {
   String relayName;
   bool canDelete;
   bool isSelected; //Has this Relay been selected to establish a connection?
@@ -35,10 +36,103 @@ class RelayModel{
     this.createTime = 0,
   });
 
-
   @override
   String toString() {
     return 'RelayModel{relayName: $relayName, canDelete: $canDelete, isSelected: $isSelected, isAddedCommend: $isAddedCommend, connectStatus: $connectStatus, createTime: $createTime}';
+  }
+
+  static List<RelayModel> checkDefaultRelays(List<String> relayAddressList) {
+    List<RelayModel> commendRelayList = [];
+    bool containsOxChatRelay = relayAddressList.contains(CommonConstant.oxChatRelay);
+    if (!containsOxChatRelay)
+      commendRelayList.add(RelayModel(
+        relayName: CommonConstant.oxChatRelay,
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: true,
+        isAddedCommend: containsOxChatRelay ? true : false,
+        createTime: DateTime.now().millisecondsSinceEpoch,
+      ));
+    bool containsYabume = relayAddressList.contains(''
+        'wss://yabu.me');
+    if (!containsYabume)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsYabume ? true : false,
+        relayName: 'wss://yabu.me',
+      ));
+    bool containsSiamstr = relayAddressList.contains(''
+        'wss://relay.siamstr.com');
+    if (!containsSiamstr)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsSiamstr ? true : false,
+        relayName: 'wss://relay.siamstr.com',
+      ));
+    bool containsDamusIo = relayAddressList.contains('wss://relay.damus.io');
+    if (!containsDamusIo)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsDamusIo ? true : false,
+        relayName: 'wss://relay.damus.io',
+      ));
+    bool containsNostrBand = relayAddressList.contains(''
+        'wss://relay.nostr.band');
+    if (!containsNostrBand)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsNostrBand ? true : false,
+        relayName: 'wss://relay.nostr.band',
+      ));
+    bool containsNoslol = relayAddressList.contains(''
+        'wss://nos.lol');
+    if (!containsNoslol)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsNoslol ? true : false,
+        relayName: 'wss://nos.lol',
+      ));
+    bool containsNostrwine = relayAddressList.contains(''
+        'wss://nostr.wine');
+    if (!containsNostrwine)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsNostrwine ? true : false,
+        relayName: 'wss://nostr.wine',
+      ));
+    bool containsCoinfundit = relayAddressList.contains(''
+        'wss://nostr.coinfundit.com');
+    if (!containsCoinfundit)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsCoinfundit ? true : false,
+        relayName: 'wss://nostr.coinfundit.com',
+      ));
+    bool containsNostrland = relayAddressList.contains(''
+        'wss://eden.nostr.land');
+    if (!containsNostrland)
+      commendRelayList.add(RelayModel(
+        canDelete: true,
+        connectStatus: 3,
+        isSelected: false,
+        isAddedCommend: containsNostrland ? true : false,
+        relayName: 'wss://eden.nostr.land',
+      ));
+    return commendRelayList;
   }
 }
 
@@ -53,14 +147,14 @@ RelayModel relayModelFomJson(Map<String, dynamic> map) {
 }
 
 Map<String, dynamic> relayModelToMap(RelayModel instance) => <String, dynamic>{
-  'relayName': instance.relayName,
-  'canDelete': instance.canDelete == true ? 1 : 0,
-  'isSelected': instance.isSelected == true ? 1 : 0,
-  'isAddedCommend': instance.isAddedCommend == true ? 1 : 0,
-  'createTime': instance.createTime,
-};
+      'relayName': instance.relayName,
+      'canDelete': instance.canDelete == true ? 1 : 0,
+      'isSelected': instance.isSelected == true ? 1 : 0,
+      'isAddedCommend': instance.isAddedCommend == true ? 1 : 0,
+      'createTime': instance.createTime,
+    };
 
-class RelayConnectStatus{
+class RelayConnectStatus {
   static final int connecting = 0;
   static final int open = 1;
   static final int closing = 2;
