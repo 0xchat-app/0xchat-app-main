e81a2e7f717d09a9b054a0ece4933552dffc4fac
ZharlieW <zhw306498704@gmail.com>
2024-02-07 13:31:49 +0800

    [Zzz][update] 1. Store the cashu db password separately.

---
+++ ox_common/lib/utils/cashu_helper.dart
@@ -0,0 +1,32 @@
+
+import 'package:ox_cache_manager/ox_cache_manager.dart';
+import 'package:nostr_core_dart/nostr.dart';
+
+class CashuHelper {
+
+  static String dbpwdKeyPre = 'cashuDBpwd';
+
+  static String dbPasswordKey(String pubkey) {
+    return '$dbpwdKeyPre$pubkey';
+  }
+
+  static _saveDBPassword(String pubkey, String pwd) async {
+    await OXCacheManager.defaultOXCacheManager.saveForeverData(dbPasswordKey(pubkey), pwd);
+  }
+
+  static Future<String> getDBPassword(String pubkey) async {
+    var pwd = await OXCacheManager.defaultOXCacheManager.getForeverData(dbPasswordKey(pubkey));
+    if (pwd == null || pwd is! String || pwd.isEmpty) {
+      // Compatible code
+      final oldPwd = await OXCacheManager.defaultOXCacheManager.getForeverData('dbpw+$pubkey');
+      if (oldPwd is String && oldPwd.isNotEmpty) {
+        pwd = oldPwd;
+      } else {
+        pwd = generateStrongPassword(16);
+      }
+      await _saveDBPassword(pubkey, oldPwd);
+    }
+
+    return pwd;
+  }
+}
