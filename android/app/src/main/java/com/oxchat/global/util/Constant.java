4172efe37efee168f935bd4bf33917dd76da170a
b-fab-michael <john1995789@gmail.com>
2024-02-29 18:36:11 +0800

    [Michael][update] 1、change share key

--- android/app/src/main/java/com/oxchat/global/util/Constant.java
+++ android/app/src/main/java/com/oxchat/global/util/Constant.java
@@ -2,5 +2,5 @@ package com.oxchat.global.util;
 
 public class Constant {
     public static final String APP_SCHEME = "oxchat";
-    public static final String APP_SCHEME_SHARE = "://shareMessageWithScheme?text=";
+    public static final String APP_SCHEME_SHARE = "://shareLinkWithScheme?text=";
 }
