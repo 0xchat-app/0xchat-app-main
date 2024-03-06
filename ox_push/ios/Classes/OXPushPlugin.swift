d450449025e0c95ec5c6836817516eed1fe36213
ZharlieW <zhw306498704@gmail.com>
2024-03-06 21:12:21 +0800

    [Zzz][update] 1. Revert channel property.

--- ox_push/ios/Classes/OXPushPlugin.swift
+++ ox_push/ios/Classes/OXPushPlugin.swift
@@ -2,18 +2,22 @@ import Flutter
 import UIKit
 
 public class OXPushPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "ox_push", binaryMessenger: registrar.messenger())
-    let instance = OXPushPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
-
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    switch call.method {
-    case "getPlatformVersion":
-      result("iOS " + UIDevice.current.systemVersion)
-    default:
-      result(FlutterMethodNotImplemented)
+    
+    public static var channel: FlutterMethodChannel?
+    
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "ox_push", binaryMessenger: registrar.messenger())
+        let instance = OXPushPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+        self.channel = channel
     }
-  }
+    
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "getPlatformVersion":
+            result("iOS " + UIDevice.current.systemVersion)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
 }
