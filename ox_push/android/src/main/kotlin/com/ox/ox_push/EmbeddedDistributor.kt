fdbf23cb76c344231464e72f27803a4557478a32
b-fab-michael <john1995789@gmail.com>
2024-03-07 18:10:47 +0800

    [Michael][update] 1、optimized  used unifiedpush FCM Rewrite-Proxy

--- ox_push/android/src/main/kotlin/com/ox/ox_push/EmbeddedDistributor.kt
+++ ox_push/android/src/main/kotlin/com/ox/ox_push/EmbeddedDistributor.kt
@@ -2,11 +2,10 @@ package com.ox.ox_push
 import android.content.Context
 import org.unifiedpush.android.foss_embedded_fcm_distributor.EmbeddedDistributorReceiver
 class EmbeddedDistributor: EmbeddedDistributorReceiver() {
-    override val googleProjectNumber = "871799832562" // This value comes from the google-services.json
+    override val googleProjectNumber = "871799832562"
 
     override fun getEndpoint(context: Context, token: String, instance: String): String {
-        // This returns the endpoint of your FCM Rewrite-Proxy
-        return "https://www.0xchat.com/FCM?v2&instance=$instance&token=$token"
+        return "Embedded-FCM/FCM?v2&instance=$instance&token=$token"
     }
 
 }
