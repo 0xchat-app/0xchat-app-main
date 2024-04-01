47f124824cbd32261019a48be3ef77d918aa7803
b-fab-michael <john1995789@gmail.com>
2024-04-01 10:34:51 +0800

    [Michael][update] 1. fix contacts tabbar name not changed; 2. optimized chat session list page code

--- ox_chat/lib/utils/widget_tool.dart
+++ ox_chat/lib/utils/widget_tool.dart
@@ -3,12 +3,13 @@ import 'package:ox_common/utils/adapt.dart';
 import 'package:ox_common/widgets/common_image.dart';
 import 'package:ox_localizable/ox_localizable.dart';
 
-Widget MyText(String content, double fontSize, Color txtColor, {TextAlign? textAlign, double? height, FontWeight fontWeight = FontWeight.w400, TextOverflow? overflow, double? letterSpacing}) {
+Widget MyText(String content, double fontSize, Color txtColor, {TextAlign? textAlign, double? height, FontWeight fontWeight = FontWeight.w400, TextOverflow? overflow, double? letterSpacing, int? maxLines}) {
   return Text(
     content,
     textAlign: textAlign,
     style: TextStyle(fontSize: Adapt.px(fontSize), color: txtColor, fontWeight: fontWeight, height: height, letterSpacing: letterSpacing),
     overflow: overflow,
+    maxLines: maxLines,
   );
 }
 
