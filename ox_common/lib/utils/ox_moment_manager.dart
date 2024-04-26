26349e343b19512afe77967f1271eee885442fdd
562455044@qq.com <562455044@qq.com>
2024-04-26 11:05:24 +0800

    [bear][update] 1.update

--- ox_common/lib/utils/ox_moment_manager.dart
+++ ox_common/lib/utils/ox_moment_manager.dart
@@ -3,17 +3,6 @@ import 'ox_userinfo_manager.dart';
 
 import 'package:chatcore/chat-core.dart';
 
-String content1 =
-    "IIUC the geohash asdfasd1f.png asdfasdf.jpg  description provided asdfasdf.mp4 here should work: https://github.com/sandwichfarm/nostr-geotags?tab=readme-ov-file#example-response  --&gt; the driver could mention the radius they want to be available at, so can the person searching, ratings could be based on WoT.  What do you think nostr:npub1arkn0xxxll4llgy9qxkrncn3vc4l69s0dz8ef3zadykcwe7ax3dqrrh43w ?nostr:note1zhps6wp7rqchwlmp8s9wq3taramg849lczhds3h4wxvdm5vccc6qxa9zr8";
-String content2 =
-    "mmmIIUC the geohash des3cription provided asdfasdf.mp4 here should work: https://github.com/sandwichfarm/nostr-geotags?tab=readme-ov-file#example-response  --&gt; the driver could mention the radius they want to be available at, so can the person searching, ratings could be based on WoT.  What do you think nostr:npub1arkn0xxxll4llgy9qxkrncn3vc4l69s0dz8ef3zadykcwe7ax3dqrrh43w ?nostr:note1zhps6wp7rqchwlmp8s9wq3taramg849lczhds3h4wxvdm5vccc6qxa9zr8";
-String content3 =
-    "IIUC the geoha2sh description provid here should work: https://github.com/sandwichfarm/nostr-geotags?tab=readme-ov-file#example-response  --&gt; the driver could mention the radius they want to be available at, so can the person searching, ratings could be based on WoT.  What do you think nostr:npub1arkn0xxxll4llgy9qxkrncn3vc4l69s0dz8ef3zadykcwe7ax3dqrrh43w ?nostr:note1zhps6wp7rqchwlmp8s9wq3taramg849lczhds3h4wxvdm5vccc6qxa9zr8";
-String content4 =
-    "the driver c123ould mention the radius they want to be available at, so can the person searching, ratings could be based on WoT.  What do you think";
-String content5 =
-    "IIUC the23 geohash asdfasdf.png asdfasdf.jpg  description provided asdfasdf.mp4 here should work: https://github.com/sandwichfarm/nostr-geotags?tab=readme-ov-file#example-response  --&gt; the driver could mention the radius they want to be available at, so can the person searching, ratings could be based on WoT.  What do you think nostr:npub1arkn0xxxll4llgy9qxkrncn3vc4l69s0dz8ef3zadykcwe7ax3dqrrh43w ?nostr:note1zhps6wp7rqchwlmp8s9wq3taramg849lczhds3h4wxvdm5vccc6qxa9zr8";
-
 abstract mixin class OXMomentObserver {
   didNewPrivateNotesCallBack(NoteDB noteDB) {}
 
@@ -41,7 +30,7 @@ class OXMomentManager {
   bool removeObserver(OXMomentObserver observer) => _observers.remove(observer);
 
   Future<void> init() async {
-    // await Moment.sharedInstance.init();
+    await Moment.sharedInstance.init();
     initLocalData();
   }
 
@@ -50,7 +39,7 @@ class OXMomentManager {
     // List<NoteDB>? contactsList = await Moment.sharedInstance.loadContactsNotes();
     // List<NoteDB>? privateList = await Moment.sharedInstance.loadPrivateNotes();
     // _changeListToMap(contactsList,contactsNotesMap);
-    _changeListToMap(_mockData());
+    // _changeListToMap(_mockData());
   }
 
   addMomentCallBack() {
@@ -64,29 +53,6 @@ class OXMomentManager {
     for(NoteDB db in list){
       privateNotesMap[db.noteId] = db;
     }
-  }
-
-  _mockData(){
-    List<String> contentList = [content1,content2,content3,content4,content5];
-    List<NoteDB> list = contentList.map((String content) {
-      var random = Random();
-      int randomNumber = random.nextInt(1001);
-      final note = NoteDB(
-        noteId: randomNumber.toString(), //event id
-        author: OXUserInfoManager.sharedInstance.currentUserInfo?.pubKey ?? '',
-        createAt: DateTime.now().millisecond,
-        content: content,
-        root: 'aa',
-        private: true,
-        replyCount: randomNumber,
-        repostCount: randomNumber,
-        reactionCount: randomNumber,
-        zapCount: randomNumber,
-        zapAmount: randomNumber,
-      );
-      return note;
-    }).toList();
-    return list;
   }
 
   void newPrivateNotesCallBack(NoteDB noteDB) {
@@ -107,20 +73,5 @@ class OXMomentManager {
     }
   }
 }
-
-
-NoteDB draftNoteDB = NoteDB(
-  noteId: '12341234', //event id
-  author: OXUserInfoManager.sharedInstance.currentUserInfo?.pubKey ?? '',
-  createAt: DateTime.now().millisecond,
-  content: content1,
-  root: 'aa',
-  private: true,
-  replyCount: 1,
-  repostCount: 1,
-  reactionCount: 1,
-  zapCount: 1,
-  zapAmount: 1,
-);
 
 
