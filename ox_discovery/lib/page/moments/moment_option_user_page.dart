2a8180f7594e74daad69515b3bafb53b0140e916
bear <562455044@qq.com>
2024-05-09 20:52:33 +0800

    [bear][add] 1.  add MomentOptionUserPage

--- ox_discovery/lib/page/moments/moment_option_user_page.dart
+++ ox_discovery/lib/page/moments/moment_option_user_page.dart
@@ -1,13 +1,25 @@
 import 'package:chatcore/chat-core.dart';
 import 'package:flutter/material.dart';
+import 'package:ox_common/model/chat_session_model.dart';
+import 'package:ox_common/model/chat_type.dart';
+import 'package:ox_common/navigator/navigator.dart';
 import 'package:ox_common/utils/adapt.dart';
+import 'package:ox_common/utils/ox_chat_binding.dart';
+import 'package:ox_common/utils/ox_userinfo_manager.dart';
 import 'package:ox_common/utils/theme_color.dart';
 import 'package:ox_common/utils/widget_tool.dart';
 import 'package:ox_common/widgets/common_appbar.dart';
+import 'package:ox_common/widgets/common_hint_dialog.dart';
+import 'package:ox_common/widgets/common_image.dart';
+import 'package:ox_common/widgets/common_loading.dart';
 import 'package:ox_common/widgets/common_network_image.dart';
+import 'package:ox_common/widgets/common_toast.dart';
 import 'package:ox_discovery/model/moment_extension_model.dart';
 import 'package:ox_discovery/model/moment_ui_model.dart';
-
+import 'package:ox_localizable/ox_localizable.dart';
+import 'package:ox_module_service/ox_module_service.dart';
+import 'package:chatcore/chat-core.dart';
+import 'package:nostr_core_dart/nostr.dart';
 import '../../enum/moment_enum.dart';
 import '../../utils/discovery_utils.dart';
 import '../../utils/moment_widgets_utils.dart';
@@ -41,11 +53,10 @@ class _MomentOptionUserPageState extends State<MomentOptionUserPage> {
   }
 
   List<NotedUIModel> _getUserList(Map<String, List<dynamic>> mapInfo) {
-    print('===list=======${mapInfo}===$ZapRecordsDB');
-    List<NoteDB> list = [];
+    List<dynamic> list = [];
     switch (widget.type) {
       case ENotificationsMomentType.zaps:
-        list = (mapInfo['zap'] as List<NoteDB>);
+        list = (mapInfo['zap'] as List<ZapRecordsDB>);
         break;
       case ENotificationsMomentType.repost:
         list = (mapInfo['repost'] as List<NoteDB>);
@@ -58,7 +69,13 @@ class _MomentOptionUserPageState extends State<MomentOptionUserPage> {
         break;
     }
 
-    return list.map((NoteDB noteDB) => NotedUIModel(noteDB: noteDB)).toList();
+    return list.map((dynamic noteDB) {
+      if(widget.type == ENotificationsMomentType.zaps){
+        String content = 'Zaps +${ZapRecordsDB.getZapAmount(noteDB.bolt11)}';
+        return NotedUIModel(noteDB: NoteDB(author: (noteDB as ZapRecordsDB).sender,content: content ));
+      }
+     return NotedUIModel(noteDB: noteDB);
+    }).toList();
   }
 
   @override
@@ -82,14 +99,19 @@ class _MomentOptionUserPageState extends State<MomentOptionUserPage> {
                 ),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
-                  children: showUserDBList
-                      .map(
-                        (NotedUIModel notedUIModel) => MomentUserItemWidget(
-                          notedUIModel: notedUIModel,
-                          type: widget.type,
-                        ),
-                      )
-                      .toList(),
+                  children: [
+                    _noDataWidget(),
+                    ...showUserDBList.map(
+                    (dynamic item) {
+                // ZapRecordsDB
+                return MomentUserItemWidget(
+                wrapNotedUIModel: widget.notedUIModel,
+                notedUIModel: item,
+                type: widget.type,
+              );
+            },
+            ).toList()
+                  ],
                 ),
               ),
             ),
@@ -98,13 +120,44 @@ class _MomentOptionUserPageState extends State<MomentOptionUserPage> {
       ),
     );
   }
+
+  Widget _noDataWidget() {
+    if(showUserDBList.isNotEmpty)  return const SizedBox();
+    return Padding(
+      padding: EdgeInsets.only(
+        top: 100.px,
+      ),
+      child: Center(
+        child: Column(
+          children: [
+            CommonImage(
+              iconName: 'icon_no_data.png',
+              width: Adapt.px(90),
+              height: Adapt.px(90),
+            ),
+            Text(
+              'No ${widget.type.text} !',
+              style: TextStyle(
+                fontSize: 16.px,
+                fontWeight: FontWeight.w400,
+                color: ThemeColor.color100,
+              ),
+            ).setPaddingOnly(
+              top: 24.px,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
 }
 
 class MomentUserItemWidget extends StatefulWidget {
+  final NotedUIModel wrapNotedUIModel;
   final NotedUIModel notedUIModel;
   final ENotificationsMomentType type;
   const MomentUserItemWidget(
-      {super.key, required this.notedUIModel, required this.type});
+      {super.key, required this.notedUIModel, required this.type,required this.wrapNotedUIModel});
 
   @override
   State<MomentUserItemWidget> createState() => _MomentUserItemWidgetState();
@@ -120,20 +173,49 @@ class _MomentUserItemWidgetState extends State<MomentUserItemWidget> {
   }
 
   void _getUserList() async {
-    if(widget.notedUIModel.noteDB.isRepost){
-      NoteDB? note = await Moment.sharedInstance.loadNoteWithNoteId(widget.notedUIModel.noteDB.repostId!);
-      if(note != null){
-        notedUIModel = NotedUIModel(noteDB: note);
-      }
-    }else{
-      notedUIModel = widget.notedUIModel;
+    if (widget.type == ENotificationsMomentType.repost) {
+      _initReposted();
     }
-    setState(() {});
-    _getUserDB(widget.notedUIModel.noteDB.author);
 
+    if (widget.type == ENotificationsMomentType.like) {
+      _initLike();
+    }
+
+    if (widget.type == ENotificationsMomentType.quote) {
+      notedUIModel = widget.notedUIModel;
+      _getUserDB(widget.notedUIModel.noteDB.author);
+    }
+
+    if (widget.type == ENotificationsMomentType.zaps) {
+      notedUIModel = widget.notedUIModel;
+      _getUserDB(widget.notedUIModel.noteDB.author);
+    }
   }
 
-  void _getUserDB(String pubKey)async{
+  void _initReposted() async {
+    NoteDB? note = await Moment.sharedInstance.loadNoteWithNoteId(widget.notedUIModel.noteDB.repostId!);
+    if (note != null) {
+      NotedUIModel newNoted = NotedUIModel(noteDB: note);
+      notedUIModel = newNoted;
+      _getUserDB(widget.notedUIModel.noteDB.author);
+    }
+  }
+
+  void _initLike() async {
+    if(widget.wrapNotedUIModel.noteDB.isRepost){
+      NoteDB? note = await Moment.sharedInstance.loadNoteWithNoteId(widget.wrapNotedUIModel.noteDB.repostId!);
+      if (note != null) {
+        NotedUIModel newNoted = NotedUIModel(noteDB: note);
+        notedUIModel = newNoted;
+
+      }
+    }else{
+      notedUIModel = widget.wrapNotedUIModel;
+    }
+    _getUserDB(widget.notedUIModel.noteDB.author);
+  }
+
+  void _getUserDB(String pubKey) async {
     UserDB? userDB = await Account.sharedInstance.getUserInfo(pubKey);
     if (userDB != null) {
       user = userDB;
@@ -141,11 +223,12 @@ class _MomentUserItemWidgetState extends State<MomentUserItemWidget> {
     }
   }
 
-
+  String get _getContent{
+    return notedUIModel?.getMomentShowContent ?? '';
+  }
 
   @override
   Widget build(BuildContext context) {
-    if (user == null || notedUIModel == null) return const SizedBox();
     return _userItemWidget();
   }
 
@@ -157,22 +240,31 @@ class _MomentUserItemWidgetState extends State<MomentUserItemWidget> {
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          MomentWidgetsUtils.clipImage(
-            borderRadius: 60.px,
-            imageSize: 60.px,
-            child: OXCachedNetworkImage(
-              imageUrl: user?.picture ?? '',
-              fit: BoxFit.cover,
-              placeholder: (context, url) =>
-                  MomentWidgetsUtils.badgePlaceholderImage(),
-              errorWidget: (context, url, error) =>
-                  MomentWidgetsUtils.badgePlaceholderImage(),
-              width: 60.px,
-              height: 60.px,
-            ),
-          ).setPaddingOnly(right: 16.px),
+          GestureDetector(
+            onTap: () {
+              if(user == null) return;
+              OXModuleService.pushPage(context, 'ox_chat', 'ContactUserInfoPage', {
+                'pubkey':user?.pubKey ?? '',
+              });
+            },
+            child: MomentWidgetsUtils.clipImage(
+              borderRadius: 60.px,
+              imageSize: 60.px,
+              child: OXCachedNetworkImage(
+                imageUrl: user?.picture ?? '',
+                fit: BoxFit.cover,
+                placeholder: (context, url) =>
+                    MomentWidgetsUtils.badgePlaceholderImage(),
+                errorWidget: (context, url, error) =>
+                    MomentWidgetsUtils.badgePlaceholderImage(),
+                width: 60.px,
+                height: 60.px,
+              ),
+            ).setPaddingOnly(right: 16.px),
+          ),
           Expanded(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Container(
                   padding: EdgeInsets.only(
@@ -204,48 +296,27 @@ class _MomentUserItemWidgetState extends State<MomentUserItemWidget> {
                           ],
                         ),
                       ),
-                      GestureDetector(
-                        behavior: HitTestBehavior.translucent,
-                        onTap: () {},
-                        child: Container(
-                          width: 90.px,
-                          height: 30.px,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(8),
-                            color: ThemeColor.color180,
-                            gradient: LinearGradient(
-                              colors: [
-                                ThemeColor.gradientMainEnd,
-                                ThemeColor.gradientMainStart,
-                              ],
-                              begin: Alignment.centerLeft,
-                              end: Alignment.centerRight,
-                            ),
-                          ),
-                          alignment: Alignment.center,
-                          child: Text(
-                            'Add',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 14.px,
-                              fontWeight: FontWeight.w400,
-                            ),
-                          ),
-                        ),
-                      )
+                      _addFriendWidget(),
                     ],
                   ),
                 ),
-                Container(
-                  child: Text(
-                    notedUIModel?.getMomentShowContent ?? '',
-                    style: TextStyle(
-                      color: ThemeColor.color0,
-                      fontSize: 12.px,
-                      fontWeight: FontWeight.w400,
+                GestureDetector(
+                  onTap: (){
+                    // if(widget.type == ENotificationsMomentType.quote){
+                    //     // OXNavigator.pushPage(context, (context) => null)
+                    // }
+                  },
+                  child: Container(
+                    child: Text(
+                      _getContent,
+                      style: TextStyle(
+                        color: ThemeColor.color0,
+                        fontSize: 12.px,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                     ),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
                   ),
                 ),
               ],
@@ -254,5 +325,82 @@ class _MomentUserItemWidgetState extends State<MomentUserItemWidget> {
         ],
       ),
     );
+  }
+
+  Widget _addFriendWidget(){
+    UserDB? userDB = user;
+    if(userDB == null || isFriend(userDB.pubKey)) return const SizedBox();
+
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: _addFriends,
+      child: Container(
+        width: 90.px,
+        height: 30.px,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: ThemeColor.color180,
+          gradient: LinearGradient(
+            colors: [
+              ThemeColor.gradientMainEnd,
+              ThemeColor.gradientMainStart,
+            ],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+        ),
+        alignment: Alignment.center,
+        child: Text(
+          'Add',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 14.px,
+            fontWeight: FontWeight.w400,
+          ),
+        ),
+      ),
+    );
+  }
+
+
+
+  void _addFriends() async {
+    UserDB? userDB = user;
+    if(userDB == null) return;
+
+    if (isFriend(userDB.pubKey) == false) {
+      OXCommonHintDialog.show(context,
+          content: Localized.text('ox_chat.add_contact_dialog_title'),
+          actionList: [
+            OXCommonHintAction.cancel(onTap: () {
+              OXNavigator.pop(context, false);
+            }),
+            OXCommonHintAction.sure(
+                text: Localized.text('ox_common.confirm'),
+                onTap: () async {
+                  OXNavigator.pop(context, true);
+                  await OXLoading.show();
+                  final OKEvent okEvent = await Contacts.sharedInstance
+                      .addToContact([userDB.pubKey]);
+                  await OXLoading.dismiss();
+                  if (okEvent.status) {
+                    OXChatBinding.sharedInstance.contactUpdatedCallBack();
+                    OXChatBinding.sharedInstance
+                        .changeChatSessionTypeAll(userDB.pubKey, true);
+                    CommonToast.instance.show(
+                        context, Localized.text('ox_chat.sent_successfully'));
+                  } else {
+                    CommonToast.instance.show(context, okEvent.message);
+                  }
+                }),
+          ],
+          isRowAction: true);
+    }
+  }
+
+
+  bool isFriend(String pubkey) {
+    UserDB? user = Contacts.sharedInstance.allContacts[pubkey];
+    return user != null;
   }
 }
