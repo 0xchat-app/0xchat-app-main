8b0696ed6056676f2332065d5980599f7a934eea
water783 <wkuangji@outlook.com>
2024-05-15 19:29:46 +0800

    fix typo

--- ox_discovery/lib/enum/visible_type.dart
+++ ox_discovery/lib/enum/visible_type.dart
@@ -2,7 +2,7 @@ enum VisibleType {
   everyone(name: 'Everyone', illustrate: 'All People'),
   allContact(name: 'My Contacts', illustrate: 'All Contacts'),
   private(name: 'Private', illustrate: 'Just me'),
-  excludeContact(name: 'Selected Friends', illustrate: 'Selected Users');
+  excludeContact(name: 'Selected Friends', illustrate: 'Only Selected Users');
   // includeContact(name: 'Selected User', illustrate: 'Just Selected Contacts');
 
   final String name;
