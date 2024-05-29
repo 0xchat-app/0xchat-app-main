79c76a8da642d647fa3edbb9319ef18c21f0dc3c
bear-Wx <562455044@qq.com>
2024-05-29 18:43:37 +0800

    [bear][add] 1. add OXDiscoveryInterface

---
+++ ox_common/lib/business_interface/ox_discovery/interface.dart
@@ -0,0 +1,15 @@
+import '../../utils/custom_uri_helper.dart';
+
+class OXDiscoveryInterface {
+  static const moduleName = 'ox_discovery';
+
+  static String getMomentPageUri(String notedId) {
+    String link = CustomURIHelper.createModuleActionURI(
+      module: 'ox_discovery',
+      action: 'momentPage',
+      params: {'noteId': notedId},
+    );
+    return link;
+  }
+}
+
