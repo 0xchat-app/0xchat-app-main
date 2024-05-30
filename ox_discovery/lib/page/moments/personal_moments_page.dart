6f3a09543b3f5c88e9758bb7f68e0df9279c468a
Justin <Justin@gmail.com>
2024-05-30 19:03:28 +0800

    [Justin][update]fix bug

--- ox_discovery/lib/page/moments/personal_moments_page.dart
+++ ox_discovery/lib/page/moments/personal_moments_page.dart
@@ -342,7 +342,10 @@ class _PersonMomentsPageState extends State<PersonMomentsPage>
 
   void _refreshData(List<NoteDB> noteList){
     List<NoteDB> filteredNoteList = noteList.where((element) => element.getNoteKind() != ENotificationsMomentType.like.kind).toList();
-    if (filteredNoteList.isEmpty) {
+    if (noteList.isEmpty) {
+      updateStateView(CommonStateView.CommonStateView_NoData);
+      _refreshController.footerStatus == LoadStatus.idle ? _refreshController.loadComplete() : _refreshController.loadNoData();
+      setState(() {});
       return;
     }
     _notes.clear();
