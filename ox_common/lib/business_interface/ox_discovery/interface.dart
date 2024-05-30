9fb730651e51a3ca55e77217853c6983f36a42aa
bear-Wx <562455044@qq.com>
2024-05-30 17:16:31 +0800

    [bear][update] 1.update function

--- ox_common/lib/business_interface/ox_discovery/interface.dart
+++ ox_common/lib/business_interface/ox_discovery/interface.dart
@@ -1,18 +1,14 @@
-import 'package:flutter/cupertino.dart';
 import '../../utils/custom_uri_helper.dart';
 
 class OXDiscoveryInterface {
   static const moduleName = 'ox_discovery';
 
-  static void openMomentPage(
-    BuildContext? context, {
-    required String notedId,
-  }) {
+  static String getJumpMomentPageUri(String notedId) {
     String link = CustomURIHelper.createModuleActionURI(
       module: 'ox_discovery',
       action: 'momentPage',
       params: {'noteId': notedId},
     );
-    link.tryHandleCustomUri(context: context);
+    return link;
   }
 }
