533ee73d0cd873d4940ce55b46a9548962eca310
water783 <wkuangji@outlook.com>
2024-06-25 11:22:27 +0800

    add upload

---
+++ ox_common/lib/upload/nostr_build_uploader.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'base64.dart';
+
+class NostrBuildUploader {
+  static var dio = Dio();
+
+  static final String UPLOAD_ACTION = "https://nostr.build/api/v2/upload/files";
+
+  static Future<String?> upload(String filePath, {String? fileName}) async {
+    MultipartFile? multipartFile;
+    if (BASE64.check(filePath)) {
+      var bytes = BASE64.toData(filePath);
+      multipartFile = MultipartFile.fromBytes(
+        bytes,
+        filename: fileName,
+      );
+    } else {
+      multipartFile = await MultipartFile.fromFile(
+        filePath,
+        filename: fileName,
+      );
+    }
+
+    var formData = FormData.fromMap({"file": multipartFile});
+    var response = await dio.post(UPLOAD_ACTION, data: formData);
+    var body = response.data;
+    if (body is Map<String, dynamic> && body["status"] == "success") {
+      return body["data"][0]["url"];
+    }
+
+    return null;
+  }
+}
