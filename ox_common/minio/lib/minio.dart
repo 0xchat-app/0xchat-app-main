3ce2930db8a1a4f8752f8d71f43fa379a365e31e
Justin <Justin@gmail.com>
2024-06-28 18:43:39 +0800

    [Justin][update]add minio library

---
+++ ox_common/minio/lib/minio.dart
@@ -0,0 +1,5 @@
+library minio;
+
+export 'src/minio.dart';
+export 'src/minio_errors.dart';
+export 'src/minio_stream.dart';
---
+++ ox_common/minio/lib/models.dart
@@ -0,0 +1,2 @@
+export 'src/minio_models_generated.dart';
+export 'src/minio_models.dart';
