f7a6afd7f1ca087cd4f68070173aa76794fd2fd7
ZharlieW <zhw306498703@gmail.com>
2024-07-05 15:25:18 +0800

    [Zzz][update] 1. Add utils method.

--- ox_common/lib/business_interface/ox_chat/utils.dart
+++ ox_common/lib/business_interface/ox_chat/utils.dart
@@ -1,4 +1,6 @@
 
+import 'dart:math';
+
 import 'package:chatcore/chat-core.dart';
 
 extension UserDBChatEx on UserDB {
@@ -18,5 +20,29 @@ extension UserDBChatEx on UserDB {
     gender = user.gender;
     area = user.area;
     dns = user.dns;
+  }
+}
+
+extension UserListChatEx on List<UserDB> {
+  String abbrDesc({
+    String noneText = '',
+    int showUserCount = 2,
+    int maxNameLength = 15,
+  }) {
+    if (this.isEmpty) return noneText;
+
+    final names = this.sublist(0, min(showUserCount, this.length))
+        .map((user) {
+      final name = user.getUserShowName();
+      if (name.length > maxNameLength) return name.replaceRange(maxNameLength - 3, name.length, '...');
+      return name;
+    }).toList().join(',');
+
+    final otherCount = max(0, this.length - showUserCount);
+    if (otherCount > 0) {
+      return names + ' and $otherCount others';
+    } else {
+      return names;
+    }
   }
 }
