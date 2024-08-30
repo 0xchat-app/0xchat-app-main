9a59a1bf4051e5d1a5092cea1d3113296cfca5dd
bear <562455044@qq.com>
2024-08-30 17:57:41 +0800

    [bear][add] 1. Group chat add people who are not friends

--- ox_chat/lib/page/contacts/contact_group_list_page.dart
+++ ox_chat/lib/page/contacts/contact_group_list_page.dart
@@ -334,7 +334,11 @@ class ContactGroupListPageState<T extends ContactGroupListPage> extends State<T>
     );
   }
 
-  void _handlingSearch(String searchQuery){
+  void _handlingSearch(String searchQuery)async {
+    if(widget.groupListAction == GroupListAction.add && Account.sharedInstance.isValidPubKey(UserDBISAR.decodePubkey(searchQuery) ?? '')){
+      _searchUser(searchQuery);
+      return;
+    }
     setState(() {
       Map<String, List<UserDBISAR>> searchResult = {};
       _groupedUserList.forEach((key, value) {
@@ -345,6 +349,22 @@ class ContactGroupListPageState<T extends ContactGroupListPage> extends State<T>
       _filteredUserList = searchResult;
     });
   }
+
+  void _searchUser(String searchQuery) async {
+
+    UserDBISAR? user = await Account.sharedInstance.getUserInfo(UserDBISAR.decodePubkey(searchQuery)!);
+    if (user == null) return;
+    String tag = user.name ?? '';
+    if(tag.isNotEmpty){
+      tag = tag.substring(0,1).toUpperCase();
+    }
+
+    _filteredUserList = {
+      tag :  [user]
+    };
+    setState(() {});
+  }
+
 
   buildViewPressed() {}
 
