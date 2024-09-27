0a03c87f0aa8dafae09c1072b1b4adcdf44c3032
water <130329555+water783@users.noreply.github.com>
2024-09-27 15:07:53 +0800

    audio encrypted

--- ox_chat/lib/utils/chat_voice_helper.dart
+++ ox_chat/lib/utils/chat_voice_helper.dart
@@ -19,10 +19,22 @@ class ChatVoiceMessageHelper {
     required ChatSessionModelISAR session,
     required types.AudioMessage message,
   }) async {
-    var sourceFile = File(message.uri);
-    if (message.fileEncryptionType == types.EncryptionType.encrypted) {
-      sourceFile = await DecryptedCacheManager.decryptFile(sourceFile, session.chatId);
+    File sourceFile;
+    String extension = message.uri.split('.').last;
+    final audioManager = OXFileCacheManager.get(encryptKey: message.decryptKey);
+    final cacheFile = await audioManager.getFileFromCache(message.uri);
+    if (cacheFile != null) {
+       sourceFile = cacheFile.file;
     }
+    else{
+      sourceFile = await audioManager.getSingleFile(message.uri);
+      if (message.fileEncryptionType == types.EncryptionType.encrypted &&
+          message.decryptKey != null) {
+        sourceFile = await DecryptedCacheManager.decryptFile(sourceFile, message.decryptKey!);
+      }
+    }
+    String newPath = sourceFile.path.replaceAll('.bin', '.$extension');
+    sourceFile = await sourceFile.rename(newPath);
     final duration = await getAudioDuration(sourceFile.path);
     return (sourceFile, duration);
   }
