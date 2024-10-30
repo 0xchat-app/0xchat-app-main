d43b953bdfc4771ffc7bbf8083f186cd658b7643
water <130329555+water783@users.noreply.github.com>
2024-10-30 16:28:16 +0800

    update sound

--- ox_common/lib/utils/chat_prompt_tone.dart
+++ ox_common/lib/utils/chat_prompt_tone.dart
@@ -3,37 +3,73 @@ import 'dart:io';
 
 import 'package:audioplayers/audioplayers.dart';
 import 'package:chatcore/chat-core.dart';
+import 'package:ox_common/utils/throttle_utils.dart';
+
+import 'ox_userinfo_manager.dart';
+
+enum SoundType { Message_Received, Message_Sent, Zap_Received, Zap_Sent }
 
 class PromptToneManager {
-
   bool Function(MessageDBISAR msg)? isCurrencyChatPage;
   bool isAppPaused = false;
 
   static final PromptToneManager sharedInstance = PromptToneManager._internal();
 
   final AudioPlayer _player;
+  final _throttle = ThrottleUtils(delay: Duration(milliseconds: 3000));
 
   PromptToneManager._internal() : _player = AudioPlayer();
 
   static AudioContext get _defaultAudioContext => AudioContextConfig(
-    respectSilence: Platform.isIOS ? true : false,
-    stayAwake: false,
-    focus: Platform.isIOS ? AudioContextConfigFocus.gain : AudioContextConfigFocus.mixWithOthers,
-  ).build();
+        respectSilence: Platform.isIOS ? true : false,
+        stayAwake: false,
+        focus:
+            Platform.isIOS ? AudioContextConfigFocus.gain : AudioContextConfigFocus.mixWithOthers,
+      ).build();
 
   Future setup() async {
     await AudioPlayer.global.setAudioContext(_defaultAudioContext);
   }
 
-  void play() async {
-    if (_player.state != PlayerState.playing) {
-      _player.setReleaseMode(ReleaseMode.release);
-      await AudioPlayer.global.setAudioContext(_defaultAudioContext);
-      _player.play(
-        AssetSource('sounds/message_notice_sound.mp3'),
-        ctx: _defaultAudioContext,
-      );
-    }
+  void playMessageReceived() async {
+    _playSound(SoundType.Message_Received);
+  }
+
+  void playMessageSent() async {
+    _playSound(SoundType.Message_Sent);
+  }
+
+  void playZapReceived() async {
+    _playSound(SoundType.Zap_Received);
+  }
+
+  void playZapSent() async {
+    _playSound(SoundType.Zap_Sent);
+  }
+
+  void _playSound(SoundType type) async {
+    if (isAppPaused || !OXUserInfoManager.sharedInstance.canSound) return;
+    _throttle(() async {
+      String source = '';
+      switch (type) {
+        case SoundType.Message_Received:
+          source = 'sounds/message-receive.mp3';
+        case SoundType.Message_Sent:
+          source = 'sounds/message-send.mp3';
+        case SoundType.Zap_Received:
+          source = 'sounds/zap-receive.mp3';
+        case SoundType.Zap_Sent:
+          source = 'sounds/zap-send.mp3';
+      }
+      if (_player.state != PlayerState.playing) {
+        _player.setReleaseMode(ReleaseMode.release);
+        await AudioPlayer.global.setAudioContext(_defaultAudioContext);
+        _player.play(
+          AssetSource(source),
+          ctx: _defaultAudioContext,
+        );
+      }
+    });
   }
 
   void playCalling() async {
@@ -48,7 +84,7 @@ class PromptToneManager {
     // There is no need to set AudioContext because WebRTC has its own playback type control
     // await AudioPlayer.global.setGlobalAudioContext(audioContext);
     _player.play(
-      AssetSource('sounds/sound_calling_3d.mp3'),
+      AssetSource('sounds/calling.mp3'),
     );
   }
 
