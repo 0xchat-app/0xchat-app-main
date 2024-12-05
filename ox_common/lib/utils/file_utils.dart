67d19a470bac31c524c5478635e282212764b239
bear <>
2024-12-05 17:42:29 +0800

    [bear][update] 1. update file

--- ox_common/lib/utils/file_utils.dart
+++ ox_common/lib/utils/file_utils.dart
@@ -2,12 +2,25 @@
 import 'dart:async';
 import 'dart:io';
 
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+import 'dart:ui' as ui;
 import 'package:archive/archive.dart';
 import 'package:flutter/services.dart';
 import 'package:file_picker/file_picker.dart';
+import 'package:ox_common/utils/platform_utils.dart';
 import 'package:pick_or_save/pick_or_save.dart';
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 
+import 'package:path_provider/path_provider.dart';
+
+import 'package:ffmpeg_kit_flutter/return_code.dart';
+
+
+import 'package:uuid/uuid.dart';
 import '../ox_common.dart';
+import 'image_picker_utils.dart';
 
 class FileUtils {
   static Future<void> unzip(String zipPath,String unzipPath) async{
@@ -111,8 +124,76 @@ class FileUtils {
       if (filePath.isNotEmpty) {
         return File(filePath);
       }
+    }else if(PlatformUtils.isDesktop){
+      FilePickerResult? result = await FilePicker.platform.pickFiles();
+      if (result != null) {
+        return File(result.files.single.path!);
+      }
     }
     return null;
+  }
+
+  static Future<List<Media>?> importClientFile(int type) async {
+    final isVideo = type == 2;
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      allowMultiple: false,
+      type: isVideo ? FileType.video : FileType.image,
+      compressionQuality : 1024,
+    );
+    List<Media> mediaList = [];
+    if(result != null) {
+        for (final item in result.files) {
+          if (item.path == null) continue;
+
+          final media = Media()
+            ..path = item.path
+            ..galleryMode = isVideo ? GalleryMode.video : GalleryMode.image;
+
+          if (isVideo) {
+            final thumbPath = await FileUtils.macosPlatformVideoExtractThumbnail(item.path!);
+
+            if (thumbPath != null) {
+              media.thumbPath = thumbPath;
+            }
+          }
+          mediaList.add(media);
+      }
+        return mediaList;
+    }
+
+    return null;
+  }
+
+  static Future<String?> macosPlatformVideoExtractThumbnail(String videoPath) async {
+    final directory = await getTemporaryDirectory();
+    final uuid = Uuid();
+    final uniqueId = uuid.v4();
+    final outputPath = '${directory.path}/thumbnail_$uniqueId.png';
+
+    print('Video Path: $videoPath');
+    print('Output Path: $outputPath');
+
+    if (!File(videoPath).existsSync()) {
+      print('Error: Video file not found at $videoPath');
+      return null;
+    }
+
+    final command = '-i "$videoPath" -vf "thumbnail,scale=640:360" -frames:v 1 "$outputPath"';
+    print('Executing command: $command');
+
+    final session = await FFmpegKit.execute(command);
+
+    final returnCode = await session.getReturnCode();
+    if (ReturnCode.isSuccess(returnCode)) {
+      print('Thumbnail created successfully at $outputPath');
+      return outputPath;
+    } else {
+      final log = await session.getAllLogsAsString();
+      final failStackTrace = await session.getFailStackTrace();
+      print('FFmpeg Error Log: $log');
+      print('Fail Stack Trace: $failStackTrace');
+      throw Exception('Failed to create thumbnail. Return code: $returnCode');
+    }
   }
 
   static FutureOr<bool?> exportFile(String filePath, [String? fileName]) async {
