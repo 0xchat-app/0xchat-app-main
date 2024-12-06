519369313d1ce8f26a5e116390b19d7c5a54bef6
ZharlieW <zhw306498704@gmail.com>
2024-12-06 15:33:19 +0800

    [Zzz][update] 1. iOS external sharing supports images and videos.

--- ios/Runner/Common/AppGroupHelper.swift
+++ ios/Runner/Common/AppGroupHelper.swift
@@ -9,7 +9,9 @@ import Foundation
 
 class AppGroupHelper {
     static let appGroupId = "group.0xchat.app"
-    static let shareDataKey = "0xchatShareTempDataKey"
+    static let shareScheme = "oxchat://shareLinkWithScheme"
+    static let shareDataURLKey = "0xchatShareTempDataKey"
+    static let shareDataFilePathKey = "0xchatShareTempFilePathKey"
     
     static func saveDataForGourp(_ value: Any?, forKey key: String) {
         guard let userDefaults = UserDefaults(suiteName: AppGroupHelper.appGroupId) else {
@@ -23,5 +25,9 @@ class AppGroupHelper {
             return nil
         }
         return userDefaults.object(forKey: key)
+    }
+    
+    static func groupContainerURL() -> URL? {
+        FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: appGroupId)
     }
 }
