bfce657b54c0cb16f682ca688b2131db8c9ed402
ZharlieW <zhw306498704@gmail.com>
2024-12-06 15:01:33 +0800

    [Zzz][update] 1. Improve generatePartialFileMd5

--- ox_common/lib/utils/encode_utils.dart
+++ ox_common/lib/utils/encode_utils.dart
@@ -4,7 +4,8 @@ import 'package:crypto/crypto.dart';
 
 class EncodeUtils {
   static Future<String> generatePartialFileMd5(File file, {int numBytes = 1024}) async {
-    final fileStream = file.openRead(0, numBytes);
+    final fileSize = await file.length();
+    final fileStream = file.openRead(fileSize - numBytes, fileSize);
     final bytes = await fileStream.fold<List<int>>([],
             (previous, element) => previous..addAll(element));
     final digest = md5.convert(bytes);
