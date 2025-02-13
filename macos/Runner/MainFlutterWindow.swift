28189ec09a827ffd5979c087ee8df3c7b368d211
water <130329555+water783@users.noreply.github.com>
2025-02-13 18:15:42 +0800

    Improve macOS window operations and add support for remembering window size and position from the last close

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -2,14 +2,20 @@ import Cocoa
 import FlutterMacOS
 
 class MainFlutterWindow: NSWindow {
-  override func awakeFromNib() {
-    let flutterViewController = FlutterViewController()
-    let windowFrame = self.frame
-    self.contentViewController = flutterViewController
-    self.setFrame(windowFrame, display: true)
-
-    RegisterGeneratedPlugins(registry: flutterViewController)
-
-    super.awakeFromNib()
-  }
+    
+    override init(contentRect: NSRect, styleMask style: NSWindow.StyleMask, backing backingStoreType: NSWindow.BackingStoreType, defer flag: Bool) {
+        super.init(contentRect: contentRect, styleMask: style, backing: backingStoreType, defer: flag)
+        initializeWindow()
+    }
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        initializeWindow()
+    }
+    
+    func initializeWindow() {
+        let flutterViewController = FlutterViewController()
+        self.contentViewController = flutterViewController
+        self.setFrame(CGRectZero, display: false)
+        RegisterGeneratedPlugins(registry: flutterViewController)
+    }
 }
