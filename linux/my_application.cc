54090d423a52914245e4ef0e0f6864ac5f2445d4
bear <>
2026-01-29 16:13:59 +0800

    Update name (linux)

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,13 +40,13 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "oxchat_app_main");
+    gtk_header_bar_set_title(header_bar, "0xchat");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "oxchat_app_main");
+    gtk_window_set_title(window, "0xchat");
   }
-
+  gdk_set_program_class("0xchat");
   gtk_window_set_default_size(window, 1280, 720);
   gtk_widget_show(GTK_WIDGET(window));
 
